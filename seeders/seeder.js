7fcd6bf519d6e163cc549e9cf43ea9f8ce5310a8
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-02-27 12:05:54 -0500

    Added Seeders

--- seeders/seeder.js
+++ seeders/seeder.js
@@ -1,7 +1,49 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-     gen
-    },
+              await queryInterface.createTable('bands', {
+            band_id: {
+              allowNull: false,
+              autoIncrement: true,
+              primaryKey: true,
+              type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                timestamps: false,
+                primaryKey: true,
+                autoIncrement: true
+                },
+            genre: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+                timestamps: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            available_start_time: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                timestamps: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            end_time: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                timestamps: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            event: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                timestamps: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+              })
+        },
   
     down: async (queryInterface, Sequelize) => {
       /**
@@ -11,4 +53,4 @@ module.exports = {
        * await queryInterface.bulkDelete('People', null, {});
        */
     }
-  };
+}
